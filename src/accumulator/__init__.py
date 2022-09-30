f8c99c2e8424a50023260e47580ac235c5951b64
Ben Maas <ben.maas@analysts.com>
2022-09-30 10:26:48 -0500

    suggestions from pylint

--- src/accumulator/__init__.py
+++ src/accumulator/__init__.py
@@ -1,1 +1,6 @@
+"""
+accumulator/__init__.py
+
+A simple Flask based REST accumulator for use as a demo.
+"""
 from .app import app
--- src/accumulator/app.py
+++ src/accumulator/app.py
@@ -1,3 +1,8 @@
+"""
+accumulator/app.py
+
+A simple Flask based REST accumulator for use as a demo.
+"""
 import json
 
 from flask import Flask, make_response
@@ -14,22 +19,23 @@ def _build_response():
     return r
 
 
-@app.route("/add/<key>/<value>")
+@app.route('/add/<key>/<value>')
 def add(key: str, value: int):
+    # pylint: disable=global-variable-not-assigned
     global data
     if key not in data:
         data[key] = 0
 
-    data[key] = data[key] + value
+    data[key] = data[key] + int(value)
 
     return _build_response()
 
 
-@app.route("/data")
+@app.route('/data')
 def get_data():
     return _build_response()
 
 
-@app.route("/increment/<key>")
+@app.route('/increment/<key>')
 def increment(key: str):
     return add(key, 1)
